1d4426fa71c33edab124598a2959d5a909434f03
Berk Cambaz <berkcambaz12321@gmail.com>
2022-08-07 18:24:20 +0300

    Add 3 events for orders & green apple

--- Assets/Scripts/OrderController.cs
+++ Assets/Scripts/OrderController.cs
@@ -6,14 +6,13 @@ using UnityEngine.UI;
 public class OrderController : MonoBehaviour
 {
     public Order order;
-    public Guest guest;
-    public Food[] foods;
+    [System.NonSerialized] public Guest guest;
+    [System.NonSerialized] public Food[] foods;
 
     [Space(10f)]
 
     public Image guestImage;
     public Image[] foodImages;
-    public Image buttonImage;
 
     void OnEnable()
     {
