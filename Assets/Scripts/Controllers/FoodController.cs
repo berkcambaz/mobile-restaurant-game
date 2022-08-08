7b28e2b0c6528f1263c68ffae3cda18248b0571a
Berk Cambaz <berkcambaz12321@gmail.com>
2022-08-08 20:00:06 +0300

    Add food card

---
+++ Assets/Scripts/Controllers/FoodController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FoodController : MonoBehaviour
+{
+    public Image foodImage;
+    public Text foodText;
+    public RectTransform starMaskRectTransform;
+    public Image goldImage;
+    public Image diamondImage;
+    public Text costText;
+
+    public void ButtonUpgrade()
+    {
+
+    }
+}
